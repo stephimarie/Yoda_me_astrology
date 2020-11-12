3e57ee4b9e9367a02ca329ad490f4c1ce062c6f4
Maxx <maxhiga.hawaii@gmail.com>
2020-11-11 16:33:16 -1000

    UPDATE: changed submit function to click function when an a tag is clicked & added popup functions for aquarius to taurus. should be all working

--- assets/app.js
+++ assets/app.js
@@ -1,28 +1,25 @@
 $(document).ready(function () {
 
     //example of image click function to get id or value from a bigger thing then input to push to api calls
-    // $("img").on("click", function () {
+    // $("a").on("click", function () {
     //     console.log($(this).attr("id"))
     // })
 
-
-
     //variable to set up submit function based on horoscope signs
-    let userInput = $("form");
+    let userInput = $("a");
 
     //submit function to call api when input is clicked
-    userInput.on("submit", function (e) {
-        e.preventDefault();
+    userInput.on("click", function () {
 
         //variable for user sign input from buttons
-        let userSign = e.target.querySelector("input").getAttribute("value");
+        let userSign = $(this).attr("id");
         console.log("user selected: " + userSign);
 
         //axios call to aztro horoscop api
         axios({
             method: "POST",
             url: "https://sameer-kumar-aztro-v1.p.rapidapi.com/",
-            params: { day: 'today', sign: 'aquarius' },
+            params: { day: 'today', sign: userSign },
             headers: {
                 "x-rapidapi-key": "0ad9d781cemshb9f1fc3ee82194cp11e268jsncdcd563df8b3",
                 "x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
@@ -35,30 +32,72 @@ $(document).ready(function () {
             console.log("horoscope says: " + response.data.description);
             let horoText = response.data.description;
 
-            //axios call to yoda translate
-            axios({
-                method: "POST",
-                url: "https://yodish.p.rapidapi.com/yoda.json",
-                headers: {
-                    "content-type": "application/x-www-form-urlencoded",
-                    "x-rapidapi-host": "yodish.p.rapidapi.com",
-                    "x-rapidapi-key": "0ad9d781cemshb9f1fc3ee82194cp11e268jsncdcd563df8b3",
-                    useQueryString: true,
-                },
-                params: {
-                    text: horoText,
-                },
-                data: {},
-            })
-                .then((response) => {
-                    console.log("Yoda says: " + response.data.contents.translated);
-                })
-                .catch((error) => {
-                    console.log("yoda translate error: " + error);
-                });
+            // axios call to yoda translate
+            // axios({
+            //     method: "POST",
+            //     url: "https://yodish.p.rapidapi.com/yoda.json",
+            //     headers: {
+            //         "content-type": "application/x-www-form-urlencoded",
+            //         "x-rapidapi-host": "yodish.p.rapidapi.com",
+            //         "x-rapidapi-key": "0ad9d781cemshb9f1fc3ee82194cp11e268jsncdcd563df8b3",
+            //         useQueryString: true,
+            //     },
+            //     params: {
+            //         text: horoText,
+            //     },
+            //     data: {},
+            // })
+            //     .then((response) => {
+            //         let yodaTranslate = response.data.contents.translated
+            //         console.log("Yoda says: " + yodaTranslate);
+            //         $("h4").text(yodaTranslate);
+            //     })
+            //     .catch((error) => {
+            //         console.log("yoda translate error: " + error);
+            //     });
 
         }).catch((err) => {
             console.log("aztro horoscope error: " + err);
         });
     });
+
+    //aquarius popup functions
+    $(".aquaTrig").click(function () {
+        $('.aquaHover').show();
+    });
+    $('.aquaHover').click(function () {
+        $('.aquaHover').hide();
+    });
+
+
+    //pisces popup functions
+    $(".piscesTrig").click(function () {
+        $('.piscesHover').show();
+    });
+    $('.piscesHover').click(function () {
+        $('.piscesHover').hide();
+    });
+
+    //aries popup functions
+    $(".ariesTrig").click(function () {
+        $('.ariesHover').show();
+    });
+    $('.ariesHover').click(function () {
+        $('.ariesHover').hide();
+    });
+
+    //taurus popup functions
+    $(".taurusTrig").click(function () {
+        $('.taurusHover').show();
+    });
+    $('.taurusHover').click(function () {
+        $('.taurusHover').hide();
+    });
+
+    $('.popupCloseButton').click(function () {
+        $('.hover_bkgr_fricc').hide();
+    });
+
 });
+
+
